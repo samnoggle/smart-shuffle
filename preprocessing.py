912c5f950c272d8531037e4b310b8382f845b864
sam <sjnoggle@millersville.edu>
2022-11-17 10:46:22 -0500

    metrics

--- preprocessing.py
+++ preprocessing.py
@@ -3,25 +3,74 @@ Creates a matrix of preprocessed data, normalized with minmax
 Also has a list of the track IDs mapped for the matrix's rows
 """
 import pandas as pd
-from annoy import AnnoyIndex
+import time
 
-# Load in the track features dataset
-tracks = pd.read_csv("data/tf_mini.csv")
 
-# Turn "mode" into int value
-tracks['mode'] = tracks['mode'].str.replace('major', '1')
-tracks['mode'] = tracks['mode'].str.replace('minor', '0')
+def loadTracks():
+    """
+    Creates a numpy array of every track from the dataset
+    (with a list of track ids for mapping)
+    """
 
-tracks["mode"] = tracks["mode"].astype(int)
+    start = time.time()
 
-# Extract track_id column into a list
-trackID = tracks['track_id'].to_list()
-tracks.drop('track_id', axis=1, inplace=True)
+    # Load in the track features dataset
+    tracks = pd.read_csv("data/tf_mini.csv")
 
-# Normalize that data!
-normal_tracks = tracks.copy()
-normal_tracks = (normal_tracks - normal_tracks.min()) / \
-    (normal_tracks.max()-normal_tracks.min())
+    # Turn "mode" into int value
+    tracks['mode'] = tracks['mode'].str.replace('major', '1')
+    tracks['mode'] = tracks['mode'].str.replace('minor', '0')
 
-# Now I have a matrix of every song, and a list mapping the track ids
+    tracks["mode"] = tracks["mode"].astype(int)
+
+    # Extract track_id column into a list
+    trackID = tracks['track_id'].to_list()
+    tracks.drop('track_id', axis=1, inplace=True)
+
+    # Normalize the data
+    # normal_tracks = tracks.copy()
+    # normal_tracks = (normal_tracks - normal_tracks.min()) / \
+    #     (normal_tracks.max()-normal_tracks.min())
+
+    end = time.time()
+    t = end - start
+    print("Loading {0} tracks took {1} seconds".format(len(trackID),t))
+
+    return (trackID, tracks)
+
+def loadSessionContext():
+
+    start = time.time()
+
+    sessions = []
+
+    # Load the session dataset 
+    data = pd.read_csv("data/log_mini.csv")
+
+    # First session ID and number of rows to start
+    entries = data['session_length'].iloc[0] 
+    position = 0
+
+
+    while(position + entries < len(data.index)):
+
+        # Make them into a subset dataset
+        subset = data[position : position + entries]
+
+        # Update to continue
+        position = position + entries
+        entries = data['session_length'].iloc[position]
+
+        # Append the session to a list of dataframes 
+        sessions.append(subset)
+    
+    end = time.time()
+    t = end - start
+    print("Loading {0} sessions took {1} seconds".format(len(sessions),t))
+
+
+    return sessions
+
+
+
 
---
+++ session.py
@@ -0,0 +1,14 @@
+"""
+    Class to store a session's data
+"""
+
+class Session:
+  def __init__(self, _id, _contextMatrix):
+    self.id = _id
+    self.contextMatrix = _contextMatrix
+
+    # Construct skipped/non-skipped arrays
+    self.tracks = 
+
+   
+    
