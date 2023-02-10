a9bec6aecd6413e10c2aae8cf700f0b97bf708b2
sam <sjnoggle@millersville.edu>
2023-02-10 14:47:43 -0500

    last

--- randomForest.py
+++ randomForest.py
@@ -21,7 +21,7 @@ def create_tree():
 
 
     # split dataset into features and target variable(not_skipped)
-    X = data.loc[:, ~data.columns.isin(['not_skipped', 'session_id', 'manLastPlay','manLastSkip', 'angleAvPlay','angleAvSkip', 'duration','release_year','us_popularity_estimate','acousticness','beat_strength','bounciness','danceability','dyn_range_mean','energy','flatness','instrumentalness','key','liveness','loudness','mechanism','mode','organism','speechiness','tempo','time_signature','valence','acoustic_vector_0','acoustic_vector_1','acoustic_vector_2','acoustic_vector_3','acoustic_vector_4','acoustic_vector_5','acoustic_vector_6','acoustic_vector_7', 'hour_of_day','day_of_week','month','premium'])]
+    X = data.loc[:, ~data.columns.isin(['not_skipped', 'session_id', 'prevSongPlayed', 'percent_skipped', 'manLastPlay','manLastSkip', 'angleAvPlay','angleAvSkip', 'duration','release_year','us_popularity_estimate','acousticness','beat_strength','bounciness','danceability','dyn_range_mean','energy','flatness','instrumentalness','key','liveness','loudness','mechanism','mode','organism','speechiness','tempo','time_signature','valence','acoustic_vector_0','acoustic_vector_1','acoustic_vector_2','acoustic_vector_3','acoustic_vector_4','acoustic_vector_5','acoustic_vector_6','acoustic_vector_7', 'hour_of_day','day_of_week','month','premium'])]
     y = data.not_skipped
 
     # Think I need to do encoding on the data for categorical string features...
@@ -67,11 +67,11 @@ def create_tree():
     # Add labels to your graph
     plt.xlabel('Feature Importance Score')
     plt.ylabel('Features')
-    plt.title("Important Features: Our Data except Manhattan & Angle Between")
+    plt.title("Important Features: Metrics except Manhattan & Angle Between")
     plt.legend()
     plt.show()
     plt.tight_layout()
-    plt.savefig('importantFeatures_OurData_NoManAng.pdf')
+    plt.savefig('importantFeatures_Metrics_NoManAng.pdf')
 
 
 
