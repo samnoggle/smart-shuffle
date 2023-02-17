8017c660bdd735fe4950d08f4d6d61da484a0be9
sam <sjnoggle@millersville.edu>
2023-02-16 19:04:17 -0500

    okkkkk

--- randomForest.py
+++ randomForest.py
@@ -3,9 +3,13 @@ Decision tree to decide if a token is a claim number or not
 """
 import pickle
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def create_tree():
@@ -23,7 +27,7 @@ def create_tree():
     # X = data.loc[:, ~data.columns.isin(['not_skipped', 'session_id', 'angleAvPlay', 'angleAvSkip', 'angleLastPlay', 'angleLastSkip', 'eucAvPlay', 'eucAvSkip', 'euclidLastPlay', 'euclidLastSkip'])]
 
     # Our Data
-    X = data.loc[:, ~data.columns.isin(['not_skipped', 'session_id', 'percent_skipped', 'prevSongPlayed', 'month', 'premium', 'hour_of_day', 'day_of_week', 'duration','release_year','us_popularity_estimate','acousticness','beat_strength','bounciness','danceability','dyn_range_mean','energy','flatness','instrumentalness','key','liveness','loudness','mechanism','mode','organism','speechiness','tempo','time_signature','valence','acoustic_vector_0','acoustic_vector_1','acoustic_vector_2','acoustic_vector_3','acoustic_vector_4','acoustic_vector_5','acoustic_vector_6','acoustic_vector_7', 'hour_of_day','day_of_week','month','premium'])]
+    X = data.loc[:, ~data.columns.isin(['not_skipped', 'session_id', 'angleAvPlay', 'angleAvSkip', 'angleLastPlay', 'angleLastSkip', 'eucAvPlay', 'eucAvSkip', 'euclidLastPlay', 'euclidLastSkip', 'duration','release_year','us_popularity_estimate','acousticness','beat_strength','bounciness','danceability','dyn_range_mean','energy','flatness','instrumentalness','key','liveness','loudness','mechanism','mode','organism','speechiness','tempo','time_signature','valence','acoustic_vector_0','acoustic_vector_1','acoustic_vector_2','acoustic_vector_3','acoustic_vector_4','acoustic_vector_5','acoustic_vector_6','acoustic_vector_7', 'hour_of_day','day_of_week','month','premium'])]
 
    # X = data.loc[:, ~data.columns.isin(['not_skipped', 'session_id', 'angleAvPlay', 'angleAvSkip', 'angleLastPlay', 'angleLastSkip', 'euclidLastPlay', 'euclidLastSkip', 'eucAvSkip', 'eucAvPlay', 'duration','release_year','us_popularity_estimate','acousticness','beat_strength','bounciness','danceability','dyn_range_mean','energy','flatness','instrumentalness','key','liveness','loudness','mechanism','mode','organism','speechiness','tempo','time_signature','valence','acoustic_vector_0','acoustic_vector_1','acoustic_vector_2','acoustic_vector_3','acoustic_vector_4','acoustic_vector_5','acoustic_vector_6','acoustic_vector_7'])]
 
@@ -51,33 +55,53 @@ def create_tree():
 
 
     # Model Accuracy, how often is the classifier correct?
+    y_pred_test = rf_tree.predict(X_test)
     print('Training Accuracy : ', metrics.accuracy_score(y_train, rf_tree.predict(X_train))*100) # accuracy on train set
-    print('Validation Accuracy : ', metrics.accuracy_score(y_test, rf_tree.predict(X_test))*100) # accuracy on test set
+    print('Validation Accuracy : ', metrics.accuracy_score(y_test, y_pred_test)*100) # accuracy on test set
 
     # Generate confusion matrix
-    y_pred_test = rf_tree.predict(X_test)
-    confusion_matrix(y_test, y_pred_test)
+    matrix = confusion_matrix(y_test, y_pred_test)
+
+    # Get and reshape confusion matrix data
+    matrix = confusion_matrix(y_test, y_pred_test)
+    matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
+
+    # Build the plot
+    plt.figure(figsize=(16,7))
+    sns.set(font_scale=1.4)
+    sns.heatmap(matrix, annot=True, annot_kws={'size':10},
+                cmap=plt.cm.Greens, linewidths=0.2)
+
+    # Add labels to the plot
+    class_names = ['Spruce/Fir', 'Lodgepole Pine', 'Ponderosa Pine', 
+                'Cottonwood/Willow', 'Aspen', 'Douglas-fir',    
+                'Krummholz']
+    tick_marks = np.arange(len(class_names))
+    tick_marks2 = tick_marks + 0.5
+    plt.xticks(tick_marks, class_names, rotation=25)
+    plt.yticks(tick_marks2, class_names, rotation=0)
+    plt.xlabel('Predicted label')
+    plt.ylabel('True label')
+    plt.title('Confusion Matrix for Random Forest Model')
+    plt.savefig('../../home/students/sjnoggle/confusionMatrix_OurData.pdf')
 
     #Visualize important features
     feature_imp = pd.Series(rf_tree.feature_importances_,index=X.columns).sort_values(ascending=False)
 
     print(feature_imp)
 
-    feature_imp = feature_imp[:10]
+    # feature_imp = feature_imp[:10]
 
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    # Creating a bar plot
-    sns.barplot(x=feature_imp, y=feature_imp.index)
-    # Add labels to your graph
-    plt.xlabel('Feature Importance Score')
-    plt.ylabel('Features')
-    plt.title("Important Features: All Metrics")
-    plt.legend()
-    plt.show()
-    plt.tight_layout()
-    plt.savefig('../../home/students/sjnoggle/newGraphs/impFeats_AllMetrics.pdf')
+    # # Creating a bar plot
+    # sns.barplot(x=feature_imp, y=feature_imp.index)
+    # # Add labels to your graph
+    # plt.xlabel('Feature Importance Score')
+    # plt.ylabel('Features')
+    # plt.title("Important Features: All Metrics")
+    # plt.legend()
+    # plt.show()
+    # plt.tight_layout()
+    # plt.savefig('../../home/students/sjnoggle/newGraphs/impFeats_AllMetrics.pdf')
 
 
 
